5a89265a4cde2b09a2acff4d5625e477ff9366aa
Krittin Kaeoaram <109333136+BossHunTerCoding@users.noreply.github.com>
2024-05-08 22:19:20 +0700

    Update

---
+++ Assets/Script/Components/For GamePlay/For Command/MusicManagerComponent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MusicManagerComponent : MonoBehaviour
+{
+    public static MusicManagerComponent instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
